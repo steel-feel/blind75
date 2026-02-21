929853e6be3c2eb9f232a75e980abe37b3e71529
Himank Jain <himankjain@yahoo.in>
2026-02-21 17:10:25 +0530

    readme update

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Solution to blind 75 (neetcode)
 ## Problems
 
 - [x] [Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)
+- [x]  [Merge Intervals](https://leetcode.com/problems/merge-intervals)
--- src/greedy/merge_intervals.rs
+++ src/greedy/merge_intervals.rs
@@ -3,7 +3,6 @@
  * Problem : 
  * Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, 
  * and return an array of the non-overlapping intervals that cover all the intervals in the input.
- * link https://leetcode.com/problems/merge-intervals
  * Example 1:
  * Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
  * Output: [[1,6],[8,10],[15,18]]
